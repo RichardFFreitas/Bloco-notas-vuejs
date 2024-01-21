0b1da8a8e7aef0158a31856c73ade5bcbf7f05ec
Richard Freitas <richardfelipelives@gmail.com>
2024-01-21 14:48:08 -0300

    configuração do global/reset css

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import Title from './components/Title.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Titulo title="Anotações" />
+    <div class="container">
+      <RouterView></RouterView>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 60px;
 }
 </style>
